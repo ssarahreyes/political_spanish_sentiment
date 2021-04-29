24176810ef9fff0f2be8bb5758ba8b0a5f24ca95
Sara Hernández Reyes <sara.shr5@gmail.com>
2021-04-29 21:07:02 +0200

    ddbb-updated

--- political_spanish_sentiment_scripts/functions.py
+++ political_spanish_sentiment_scripts/functions.py
@@ -8,158 +8,22 @@ from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 
-def tweets_f1(account):
-    """
-    This function extract a data frame with the tweets of an account
-    and filter the data frame with the columns selected.
-    """
-    # extracting the tweets
-
-    CONSUMER_KEY = '5lXBIUCjAkOnbIvMlMJn7dlaS'
-    CONSUMER_SECRET = 'qeRlxcGaTmbCkNk1bMtsSlU0tVCAFL928wH8cqPi8VJX4mqkLh'
-    ACCESS_TOKEN = '787642138263683072-JnFO9ahRyIldLnmVCpfx4EP61Z33Jnr'
-    ACCESS_TOKEN_SECRET = 'FPPnZwmV1f6VqqcYzzMtXtxO9TL0wxf2TwNxYMkhfFE6k'
-    BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAALNMOAEAAAAAP%2BesQx5avGgJJ93HpEfmJ5Itg0g%3Df9o5pCoaONvJDna12ko5StopuNOPFZeJrVgd3ykj83Z38IzkiJ'
-
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
-    tweets = [tweet for tweet in tweepy.Cursor(api.user_timeline,
-                                               screen_name=account,
-                                               tweet_mode='extended',
-                                               exclude_replies=True,
-                                               include_rts=False).items(1000)]
-    # creating the data frame
-    tweets_json = [tweet._json for tweet in tweets]
-    df_tweets = pd.json_normalize(tweets_json)
-
-    # selecting useful columns
-    columns_selected = ['user.name', 'created_at', 'id', 'full_text', 'display_text_range',
-                        'source', 'retweet_count', 'favorite_count', 'user.followers_count',
-                        'user.friends_count', 'user.statuses_count']
-
-    df_tweets_filtered = df_tweets[columns_selected]
-
-    # cleaning date time
-    df_tweets_filtered.loc[:, 'created_at'] = pd.to_datetime(df_tweets_filtered['created_at'])
-
-    # cleaning source of the tweet
-    list_sources = list(df_tweets_filtered['source'])
-    df_tweets_filtered.loc[:, 'source'] = [re.findall(r'\>(.*?)\<', s) for s in list_sources]
-
-    return df_tweets_filtered
-
-
-def adding_party_column(df, string):
-    df['partido'] = string
-    return df
-
-def adding_type_post_column(df, string):
-    """
-    string = publicación o mención
-    """
-    df['tipo de post'] = string
-    return df
-
-
-def extract_hashtags(column):
-    """
-    Input: column with all tweets. ['full_text'] in this case.
-    Output: a list with used hashtags.
-    """
-    list_hashtags = list(column)
-    hashtags = [re.findall(r"#(\w+)", tweet) for tweet in list_hashtags]
-
-    return hashtags
-
-
-def adding_campaign_column(df, string):
-    """
-    Input: string = General or Madrid.
-    Output: a column with the name of the person.
-    """
-    df['campaña'] = string
-    return df
-
-
-def mentions_f1(query):
-    """
-    This function extract a data frame with the result of a query.
-    """
-
-    CONSUMER_KEY = '5lXBIUCjAkOnbIvMlMJn7dlaS'
-    CONSUMER_SECRET = 'qeRlxcGaTmbCkNk1bMtsSlU0tVCAFL928wH8cqPi8VJX4mqkLh'
-    ACCESS_TOKEN = '787642138263683072-JnFO9ahRyIldLnmVCpfx4EP61Z33Jnr'
-    ACCESS_TOKEN_SECRET = 'FPPnZwmV1f6VqqcYzzMtXtxO9TL0wxf2TwNxYMkhfFE6k'
-    BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAALNMOAEAAAAAP%2BesQx5avGgJJ93HpEfmJ5Itg0g%3Df9o5pCoaONvJDna12ko5StopuNOPFZeJrVgd3ykj83Z38IzkiJ'
-
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
-    mentions = [tweet for tweet in tweepy.Cursor(api.search,
-                                                 q=query + ' -filter:retweets',
-                                                 lang="es",
-                                                 tweet_mode='extended',
-                                                 result_type="recent").items(5000)]
-    mentions_json = [tweet._json for tweet in mentions]
-    df_mentions = pd.json_normalize(mentions_json)
-
-    # selecting useful columns
-    columns_selected = ['user.name', 'created_at', 'id', 'full_text', 'display_text_range',
-                        'source', 'retweet_count', 'favorite_count', 'user.followers_count',
-                        'user.friends_count', 'user.statuses_count']
-
-    df_mentions_filtered = df_mentions[columns_selected]
-
-    # cleaning date time
-    df_mentions_filtered['created_at'] = pd.to_datetime(df_mentions_filtered['created_at'])
-
-    # cleaning source of the tweet
-    list_sources = list(df_mentions_filtered['source'])
-    df_mentions_filtered['source'] = [re.findall(r'\>(.*?)\<', s) for s in list_sources]
-
-    return df_mentions_filtered
-
-
-def sentiment_classificaton(df, column):
-    model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
-
-    list_tweets = column.tolist()
-
-    results = classifier(list_tweets[:8000])
-
-    sentiment_list = []
-    scores = []
-
-    for result in results:
-        scores_list = result['score']
-        scores.append(scores_list)
-
-        for i in range(len(result)):
-            results_list = result['label'][i]
-            if results_list != " ":
-                sentiment_list.append(results_list)
-
-    df['sentimiento'] = sentiment_list
-    df['scores'] = scores
-
-    return df
-
-
 # UPDATING BBD TWEETS
 
-def extracting_tweets(df, account):
+def extracting_tweets(df, account, string_party, string_post_type, string_campaign):
     """
     This function extract a data frame with the tweets of an account
     and filter the data frame with the columns selected.
+    Input:
+    - df: bbdd
+    - account: twitter account.
+    - string_party: 'Partido Popular', 'PSOE'...
+    - string_post_type: 'publicación'
+    - string_campaing: 'general' or 'madrid'
+    Output: a data frame with the last tweets of the accoount indicated.
     """
 
-    # api connection
+    # api
     CONSUMER_KEY = '5lXBIUCjAkOnbIvMlMJn7dlaS'
     CONSUMER_SECRET = 'qeRlxcGaTmbCkNk1bMtsSlU0tVCAFL928wH8cqPi8VJX4mqkLh'
     ACCESS_TOKEN = '787642138263683072-JnFO9ahRyIldLnmVCpfx4EP61Z33Jnr'
@@ -172,7 +36,7 @@ def extracting_tweets(df, account):
 
     # parameters
     last_tweet_id = max(df['id'])
-    count = 100
+    count = 200
 
     # extracting the tweets
     tweets = [tweet for tweet in tweepy.Cursor(api.user_timeline,
@@ -199,6 +63,20 @@ def extracting_tweets(df, account):
     list_sources = list(df_tweets_filtered['source'])
     df_tweets_filtered.loc[:, 'source'] = [re.findall(r'\>(.*?)\<', s) for s in list_sources]
 
+    # adding a column with the party
+    df_tweets_filtered['partido'] = string_party
+
+    # adding a column with the type of post (publicación o mención)
+    df_tweets_filtered['tipo de post'] = string_post_type
+
+    # adding a column with the type of post (publicación o mención)
+    df_tweets_filtered['campaña'] = string_campaign
+
+    # extract hashtags (column ['full_text'])
+    list_hashtags = list(df_tweets_filtered['full_text'])
+    hashtags = [re.findall(r"#(\w+)", tweet) for tweet in list_hashtags]
+    df_tweets_filtered['hashtags'] = hashtags
+
     return df_tweets_filtered
 
 
@@ -221,15 +99,16 @@ def extracting_mentions(query):
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
     # date and amount information
-    count = 10000
+    count = 15000
 
     today = datetime.date.today()
     yest = today - datetime.timedelta(days=1)
     yest2 = today - datetime.timedelta(days=2)
+    yest3 = today - datetime.timedelta(days=3)
     week = today - datetime.timedelta(days=7)
     tomorrow = today + datetime.timedelta(days=1)
     since = week
-    until = yest
+    until = yest3
 
     print(f'Until: {until}')
     print(f'Since: {since}')
@@ -264,4 +143,32 @@ def extracting_mentions(query):
 
     return df_mentions_filtered
 
+
+# MACHINE LEARNING CLASSIFICATION: 1, 2, 3, 4 OR 5 STARTS
+def sentiment_classificaton(df, column):
+    model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
+
+    list_tweets = column.tolist()
+
+    results = classifier(list_tweets)
+
+    sentiment_list = []
+    scores = []
+
+    for result in results:
+        scores_list = result['score']
+        scores.append(scores_list)
+
+        for i in range(len(result)):
+            results_list = result['label'][i]
+            if results_list != " ":
+                sentiment_list.append(results_list)
+
+    df['sentimiento'] = sentiment_list
+    df['scores'] = scores
+
+    return df
 
--- political_spanish_sentiment_scripts/main.py
+++ political_spanish_sentiment_scripts/main.py
@@ -2,16 +2,16 @@ import pandas as pd
 import tweepy
 import functions as fc
 
-
+""""
 # MENTIONS
 
 print('Extracting Vox mentions...')
 
 # Menciones VOX General
-vox_query = """
+vox_query = "
 @vox_es OR "vox" OR "los de vox" OR "el de VOX" OR "la de VOX"
 OR "Santiago Abascal" OR "Rocío Monasterio" -filter:retweets
-"""
+"
 df_vox_mentions = fc.extracting_mentions(vox_query)
 df_vox_mentions = fc.adding_party_column(df_vox_mentions, 'VOX')
 df_vox_mentions = fc.adding_type_post_column(df_vox_mentions, 'mención')
@@ -47,10 +47,10 @@ df_pp_mentions.to_csv('political_spanish_sentiment_ddbb.csv', mode='a', header=F
 print('Extracting mentions of PSOE...')
 
 # Menciones PSOE General
-psoe_query = """
+psoe_query = "
 @psoe OR "partido socialista" OR "Partido Socialista Obrero Español" 
 OR "el psoe" OR "los del psoe" OR "Pedro Sánchez" OR "Ángel Gabilondo" -filter:retweets
-"""
+"
 df_psoe_mentions = fc.extracting_mentions(psoe_query)
 df_psoe_mentions = fc.adding_party_column(df_psoe_mentions, 'PSOE')
 df_psoe_mentions = fc.adding_type_post_column(df_psoe_mentions, 'mención')
@@ -62,10 +62,10 @@ df_psoe_mentions.to_csv('political_spanish_sentiment_ddbb.csv', mode='a', header
 print('Extracting mentions of Ciudadanos...')
 
 # Menciones CIUDADANOS General
-ciudadanos_query = """
+ciudadanos_query = "
 @CiudadanosCs OR "ciudadanos" OR "los de ciudadanos" OR "Inés Arrimadas"
 OR "Edmundo Bal" -filter:retweets
-"""
+"
 df_ciudadanos_mentions = fc.extracting_mentions(ciudadanos_query)
 df_ciudadanos_mentions = fc.adding_party_column(df_ciudadanos_mentions, 'CIUDADANOS')
 df_ciudadanos_mentions = fc.adding_type_post_column(df_ciudadanos_mentions, 'mención')
@@ -78,10 +78,10 @@ print('Extracting mentions of Podemos...')
 
 # Menciones PODEMOS General
 
-podemos_query = """
+podemos_query = "
 @PODEMOS OR "podemos" OR "los de podemos" OR "podemitas" OR "el de podemos" -filter:retweets
 OR "el coletas" OR "Pablo Iglesias" OR "el iglesias"
-"""
+"
 df_podemos_mentions = fc.extracting_mentions(podemos_query)
 df_podemos_mentions = fc.adding_party_column(df_podemos_mentions, 'PODEMOS')
 df_podemos_mentions = fc.adding_type_post_column(df_podemos_mentions, 'mención')
@@ -99,173 +99,91 @@ frames_mentions = [df_pp_mentions, df_psoe_mentions, df_podemos_mentions,
 
 df_final_mentions = pd.concat(frames_mentions)
 
-df_final_mentions.to_csv('political_spanish_sentiment_mentions_all.csv')
-""""
+df_final_mentions.to_csv('political_spanish_sentiment_mentions_all.csv', mode='a', header=False)
+
+
 
 # TWEETS OF PARTIES -> GENERAL
 
+"""
+
+print('reading the data frame...')
+
+df_bbdd = pd.read_csv('political_spanish_sentiment_ddbb.csv')
+
+"""
 print('Extracting tweets of parties...')
 
+
 # Partido Popular (General)
-df_pp = fc.extracting_tweets('populares')
-fc.adding_party_column(df_pp, 'Partido Popular')
-fc.adding_type_post_column(df_pp, 'publicación')
-df_pp['hashtags'] = fc.extract_hashtags(df_pp['full_text'])
-fc.adding_campaign_column(df_pp, 'general')
+df_pp = fc.extracting_tweets(df_bbdd, 'populares', 'Partido Popular', 'publicación', 'general')
 
 # PSOE (General)
-df_psoe = fc.extracting_tweets('psoe')
-fc.adding_party_column(df_psoe, 'PSOE')
-fc.adding_type_post_column(df_psoe, 'publicación')
-df_psoe['hashtags'] = fc.extract_hashtags(df_psoe['full_text'])
-fc.adding_campaign_column(df_psoe, 'general')
+df_psoe = fc.extracting_tweets(df_bbdd, 'psoe', 'PSOE', 'publicación', 'general')
 
 # PODEMOS (General)
-df_podemos = fc.extracting_tweets('PODEMOS')
-fc.adding_party_column(df_podemos, 'PODEMOS')
-fc.adding_type_post_column(df_podemos, 'publicación')
-df_podemos['hashtags'] = fc.extract_hashtags(df_podemos['full_text'])
-fc.adding_campaign_column(df_podemos, 'general')
+df_podemos = fc.extracting_tweets(df_bbdd, 'PODEMOS', 'PODEMOS', 'publicación', 'general')
 
 # CIUDADANOS (General)
-df_ciudadanos = fc.extracting_tweets('ciudadanoscs')
-fc.adding_party_column(df_ciudadanos, 'CIUDADANOS')
-fc.adding_type_post_column(df_ciudadanos, 'publicación')
-df_ciudadanos['hashtags'] = fc.extract_hashtags(df_ciudadanos['full_text'])
-fc.adding_campaign_column(df_ciudadanos, 'general')
+df_ciudadanos = fc.extracting_tweets(df_bbdd, 'ciudadanoscs', 'CIUDADANOS', 'publicación', 'general')
 
 # VOX (General)
-df_vox = fc.extracting_tweets('vox_es')
-fc.adding_party_column(df_vox, 'VOX')
-fc.adding_type_post_column(df_vox, 'publicación')
-df_vox['hashtags'] = fc.extract_hashtags(df_vox['full_text'])
-fc.adding_campaign_column(df_vox, 'general')
+df_vox = fc.extracting_tweets(df_bbdd, 'vox_es', 'VOX', 'publicación', 'general')
+
 
 # TWEETS OF PARTIES -> MADRID
 
 # Partido Popular (Madrid)
-df_pp_madrid = fc.extracting_tweets('ppmadrid')
-fc.adding_party_column(df_pp_madrid, 'Partido Popular')
-fc.adding_type_post_column(df_pp_madrid, 'publicación')
-df_pp_madrid['hashtags'] = fc.extract_hashtags(df_pp_madrid['full_text'])
-fc.adding_campaign_column(df_pp_madrid, 'madrid')
+df_pp_madrid = fc.extracting_tweets(df_bbdd, 'ppmadrid', 'Partido Popular', 'publicación', 'madrid')
 
 # PSOE (Madrid)
-df_psoe_madrid = fc.extracting_tweets('psoe_m')
-fc.adding_party_column(df_psoe_madrid, 'PSOE')
-fc.adding_type_post_column(df_psoe_madrid, 'publicación')
-df_psoe_madrid['hashtags'] = fc.extract_hashtags(df_psoe_madrid['full_text'])
-fc.adding_campaign_column(df_psoe_madrid, 'madrid')
+df_psoe_madrid = fc.extracting_tweets(df_bbdd, 'psoe_m', 'PSOE', 'publicación', 'madrid')
 
 # PODEMOS (Madrid)
-df_podemos_madrid = fc.extracting_tweets('podemosmad')
-fc.adding_party_column(df_podemos_madrid, 'PODEMOS')
-fc.adding_type_post_column(df_podemos_madrid, 'publicación')
-df_podemos_madrid['hashtags'] = fc.extract_hashtags(df_podemos_madrid['full_text'])
-fc.adding_campaign_column(df_podemos_madrid, 'madrid')
+df_podemos_madrid = fc.extracting_tweets(df_bbdd, 'podemosmad', 'PODEMOS', 'publicación', 'madrid')
 
 # CIUDADANOS (Madrid)
-df_ciudadanos_madrid = fc.extracting_tweets('CsMadridCiudad')
-fc.adding_party_column(df_ciudadanos_madrid, 'CIUDADANOS')
-fc.adding_type_post_column(df_ciudadanos_madrid, 'publicación')
-df_ciudadanos_madrid['hashtags'] = fc.extract_hashtags(df_ciudadanos_madrid['full_text'])
-fc.adding_campaign_column(df_ciudadanos_madrid, 'madrid')
+df_ciudadanos_madrid = fc.extracting_tweets(df_bbdd, 'CsMadridCiudad', 'CIUDADANOS', 'publicación', 'madrid')
 
 # Más Madrid (Madrid)
-df_masmad_madrid = fc.extracting_tweets('MasMadridCM')
-fc.adding_party_column(df_masmad_madrid, 'Más Madrid')
-fc.adding_type_post_column(df_masmad_madrid, 'publicación')
-df_masmad_madrid['hashtags'] = fc.extract_hashtags(df_masmad_madrid['full_text'])
-fc.adding_campaign_column(df_masmad_madrid, 'madrid')
+df_masmad_madrid = fc.extracting_tweets(df_bbdd, 'MasMadridCM', 'Más Madrid', 'publicación', 'madrid')
 
 # VOX (Madrid)
-df_vox_madrid = fc.extracting_tweets('VOX_AytoMadrid')
-fc.adding_party_column(df_vox_madrid, 'VOX')
-fc.adding_type_post_column(df_vox_madrid, 'publicación')
-df_vox_madrid['hashtags'] = fc.extract_hashtags(df_vox_madrid['full_text'])
-fc.adding_campaign_column(df_vox_madrid, 'madrid')
+df_vox_madrid = fc.extracting_tweets(df_bbdd, 'VOX_AytoMadrid', 'VOX', 'publicación', 'madrid')
 
 # TWEETS OF CANDIDATES AND SECRETARIES
 
 print('Extracting tweets of leaders...')
 
 # Pablo Casado (Partido Popular) (General)
-df_pcasado = fc.extracting_tweets('pablocasado_')
-fc.adding_party_column(df_pcasado, 'Partido Popular')
-fc.adding_type_post_column(df_pcasado, 'publicación')
-df_pcasado['hashtags'] = fc.extract_hashtags(df_pcasado['full_text'])
-fc.adding_campaign_column(df_pcasado, 'general')
+df_pcasado = fc.extracting_tweets(df_bbdd, 'pablocasado_', 'Partido Popular', 'publicación', 'madrid')
 
 # Pedro Sánchez (PSOE) (General)
-df_psanchez = fc.extracting_tweets('sanchezcastejon')
-fc.adding_party_column(df_psanchez, 'PSOE')
-fc.adding_type_post_column(df_psanchez, 'publicación')
-df_psanchez['hashtags'] = fc.extract_hashtags(df_psanchez['full_text'])
-fc.adding_campaign_column(df_psanchez, 'general')
+df_psanchez = fc.extracting_tweets(df_bbdd, 'sanchezcastejon', 'PSOE', 'publicación', 'madrid')
 
 # Pablo Iglesias (PODEMOS) (General)
-df_piglesias = fc.extracting_tweets('PabloIglesias')
-fc.adding_party_column(df_piglesias, 'PODEMOS')
-fc.adding_type_post_column(df_piglesias, 'publicación')
-df_piglesias['hashtags'] = fc.extract_hashtags(df_piglesias['full_text'])
-fc.adding_campaign_column(df_piglesias, 'general')
+df_piglesias = fc.extracting_tweets(df_bbdd, 'PabloIglesias', 'PODEMOS', 'publicación', 'general')
 
 # Inés Arrimadas (CIUDADANOS) (General)
-df_iarrimadas = fc.extracting_tweets('InesArrimadas')
-fc.adding_party_column(df_iarrimadas, 'CIUDADANOS')
-fc.adding_type_post_column(df_iarrimadas, 'publicación')
-df_iarrimadas['hashtags'] = fc.extract_hashtags(df_iarrimadas['full_text'])
-fc.adding_campaign_column(df_iarrimadas, 'general')
+df_iarrimadas = fc.extracting_tweets(df_bbdd, 'InesArrimadas', 'CIUDADANOS', 'publicación', 'general')
 
 # Santiago Abascal (VOX) (General)
-df_abascal = fc.extracting_tweets('Santi_ABASCAL')
-fc.adding_party_column(df_abascal, 'VOX')
-fc.adding_type_post_column(df_abascal, 'publicación')
-df_abascal['hashtags'] = fc.extract_hashtags(df_abascal['full_text'])
-fc.adding_campaign_column(df_abascal, 'general')
+df_abascal = fc.extracting_tweets(df_bbdd, 'Santi_ABASCAL', 'VOX', 'publicación', 'general')
 
 # Isabel Díaz Ayuso (Partido Popular) (Madrid)
-df_ayuso = fc.extracting_tweets('IdiazAyuso')
-fc.adding_party_column(df_ayuso, 'Partido Popular')
-fc.adding_type_post_column(df_ayuso, 'publicación')
-df_ayuso['hashtags'] = fc.extract_hashtags(df_ayuso['full_text'])
-fc.adding_campaign_column(df_ayuso, 'madrid')
-
-# Isabel Díaz Ayuso (Partido Popular) (Madrid)
-df_ayuso = fc.extracting_tweets('IdiazAyuso')
-fc.adding_party_column(df_ayuso, 'CIUDADANOS')
-fc.adding_type_post_column(df_ayuso, 'publicación')
-df_ayuso['hashtags'] = fc.extract_hashtags(df_ayuso['full_text'])
-fc.adding_campaign_column(df_ayuso, 'madrid')
+df_ayuso = fc.extracting_tweets(df_bbdd, 'IdiazAyuso', 'Partido Popular', 'publicación', 'madrid')
 
 # Ángel Gabilondo (PSOE) (Madrid)
-df_gabilondo = fc.extracting_tweets('equipoGabilondo')
-fc.adding_party_column(df_gabilondo, 'CIUDADANOS')
-fc.adding_type_post_column(df_gabilondo, 'publicación')
-df_gabilondo['hashtags'] = fc.extract_hashtags(df_gabilondo['full_text'])
-fc.adding_campaign_column(df_gabilondo, 'madrid')
+df_gabilondo = fc.extracting_tweets(df_bbdd, 'equipoGabilondo', 'PSOE', 'publicación', 'madrid')
 
 # Rocío Monasterio (VOX) (Madrid)
-df_monasterio = fc.extracting_tweets('monasterioR')
-fc.adding_party_column(df_monasterio, 'VOX')
-fc.adding_type_post_column(df_monasterio, 'publicación')
-df_monasterio['hashtags'] = fc.extract_hashtags(df_monasterio['full_text'])
-fc.adding_campaign_column(df_monasterio, 'madrid')
+df_monasterio = fc.extracting_tweets(df_bbdd, 'monasterioR', 'VOX', 'publicación', 'madrid')
 
 # Mónica García (Más Madrid) (Madrid)
-df_mgarcia = fc.extracting_tweets('Monica_Garcia_G')
-fc.adding_party_column(df_mgarcia, 'Más Madrid')
-fc.adding_type_post_column(df_mgarcia, 'publicación')
-df_mgarcia['hashtags'] = fc.extract_hashtags(df_mgarcia['full_text'])
-fc.adding_campaign_column(df_mgarcia, 'madrid')
+df_mgarcia = fc.extracting_tweets(df_bbdd, 'Monica_Garcia_G', 'Más Madrid', 'publicación', 'madrid')
 
 # Edmundo (Ciudadanos) (Madrid)
-df_edmundo = fc.extracting_tweets('BalEdmundo')
-fc.adding_party_column(df_edmundo, 'CIUDADANOS')
-fc.adding_type_post_column(df_edmundo, 'publicación')
-df_edmundo['hashtags'] = fc.extract_hashtags(df_edmundo['full_text'])
-fc.adding_campaign_column(df_edmundo, 'madrid')
-
+df_edmundo = fc.extracting_tweets(df_bbdd, 'BalEdmundo', 'CIUDADANOS', 'publicación', 'madrid')
 
 # DATA FRAME ALL TWEETS
 
@@ -279,31 +197,21 @@ frames_tweets = [df_pp, df_psoe, df_ciudadanos, df_podemos, df_vox,
 df_final_tweets = pd.concat(frames_tweets)
 df_final_tweets.to_csv('political_spanish_sentiment_tweets.csv', mode='a', header=False)
 
-print('df_final_tweets exported!')
+# UPDATING THE FINAL DATA BASE WITH TWEETS
+df_final_tweets.to_csv('political_spanish_sentiment_ddbb.csv', mode='a', header=False)
 
+print('df_final_tweets and ddbb updated!')
 
-# FINAL DATA FRAME
-final_frames = [df_final_mentions, df_final_tweets]
-df_total = pd.concat(final_frames)
-
-df_total.to_csv('political_spanish_sentiment_ddbb.csv')
-
-print('Final data frame exported!')
-
-
-# MACHINE LEARNING SENTIMENT
-#print('Sentiment Analysis...')
-#fc.sentiment_classificaton(df_total, df_total['full_text'])
-#df_total.to_csv('political_spanish_sentiment_ddbb.csv')
 
 """
 
+# MACHINE LEARNING SENTIMENT
+print('Applying ML to Sentiment Analysis...')
+df_ml = fc.sentiment_classificaton(df_bbdd, df_bbdd['full_text'])
+df_ml.to_csv('political_spanish_sentiment_ddbb_ml.csv')
+
 print('Finished!')
 
-# solve the loc problem
-# bigger: 3.000 tweets and 10.000 mentions (¿tope por día?)
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('PyCharm')
+    print('Pipeline finished')
