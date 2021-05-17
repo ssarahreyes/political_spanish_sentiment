25f5c606749510b0f13c72b2dfba9482ede6d4f5
Sara Hernández Reyes <sara.shr5@gmail.com>
2021-05-17 18:59:04 +0200

    streamlit

---
+++ political_spanish_sentiment_scripts/streamlit.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+header = st.beta_container()
+dataset = st.beta_container()
+features = st.beta_container()
+model = st.beta_continer()
+
+with header:
+    st.Title("Sentimiento político")
+    
+
